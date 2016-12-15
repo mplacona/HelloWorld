d86b26dfb3078e86d9a2e414b54bd5f184ac0f6a
mplacona <marcos.placona@gmail.com>
2016-12-15 20:14:19 +0000

    Add JNI secure information

---
+++ app/src/main/cpp/native-lib.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Marcos Placona on 15/12/2016.
+//
+
+#include "native-lib.h"
---
+++ app/src/main/jni/native-lib.c
@@ -0,0 +1,11 @@
+#include <jni.h>
+
+JNIEXPORT jstring JNICALL
+Java_info_androidsecurity_helloworld_MainActivity_invokeNativeFunction(JNIEnv *env,
+                                                                       jobject instance) {
+
+    // TODO
+
+
+    return (*env)->NewStringUTF(env, returnValue);
+}
